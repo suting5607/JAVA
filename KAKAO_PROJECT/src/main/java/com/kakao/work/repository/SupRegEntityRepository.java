4361a47c27cbc57cdc4d5e70a1e8f50e983572fc
PARKYONG <PARKYONG@LAPTOP-7PROISKD>
2019-03-18 23:45:07 +0900

    주석추가

--- KAKAO_PROJECT/src/main/java/com/kakao/work/repository/SupRegEntityRepository.java
+++ KAKAO_PROJECT/src/main/java/com/kakao/work/repository/SupRegEntityRepository.java
@@ -6,7 +6,9 @@ import com.kakao.work.entity.SupRegEntity;
 
 
 public interface SupRegEntityRepository extends JpaRepository<SupRegEntity, Long> {
-	
+	/**
+	 *SupRegEntity 테이블 저장을 위한 리파지토리
+	 **/
 	public SupRegEntity findByRegion(String region);
 
 }
